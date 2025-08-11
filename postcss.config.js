2ed893dccdc5d3bb9baeb67ff1067dcca2545cc5
Ipsita Dutta <157873351+ipsita5-dut@users.noreply.github.com>
2025-08-11 20:22:24 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
-// module.exports = {
-//   plugins: {
-//     tailwindcss: {},
-//     autoprefixer: {},
-//   },
-// }
-
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
-
-export default config;
