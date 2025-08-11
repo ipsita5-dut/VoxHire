54de10a87e1d0b85785f7a5b3bbab52f47f06336
Ipsita Dutta <157873351+ipsita5-dut@users.noreply.github.com>
2025-08-11 21:19:53 +0530

    Update and rename postcss.config.js to postcss.config.mjs

---
+++ postcss.config.mjs
@@ -0,0 +1,19 @@
+// // module.exports = {
+// //   plugins: {
+// //     '@tailwindcss/postcss': {},
+// //     autoprefixer: {},
+// //   },
+// // };
+
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
+
